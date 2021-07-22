de2f11791f14f4c148e1fa5c6c0215761449e4ad
Prafulla <Prafulla.Borse@landisgyr.com>
2021-07-22 18:10:53 +0530

    Commit ShopBridge Web App
    Identification: 12345
    Change-Id: I080e683c1686f8c5121550d2f607717471cb564a

---
+++ WebApplication1/Controllers/ItemController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ItemController : Controller
+    {
+         itemsDBEntities db = new itemsDBEntities();
+
+        // GET: api/Item
+        public async Task<ActionResult> GetItem()
+        {
+            return View(await db.Items.ToListAsync());
+        }
+
+        public ActionResult CreateItem()
+        {
+            return View(new Item());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateItem(Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Items.Add(item);
+                await db.SaveChangesAsync();
+                TempData["SuccessMessage"] = "Saved Successfully";
+                return RedirectToAction("GetItem");
+            }
+            else
+            {
+                return View(item);
+            }
+            }
+
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = await db.Items.FindAsync(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Item item = await db.Items.FindAsync(id);
+
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(Item item)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                TempData["SuccessMessage"] = "Saved Successfully";
+                return RedirectToAction("GetItem");
+            }
+            return View(item);
+        }
+
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Item item = await db.Items.FindAsync(id);
+
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id)
+        {
+            Item item = await db.Items.FindAsync(id);
+            db.Items.Remove(item);
+            await db.SaveChangesAsync();
+            TempData["SuccessMessage"] = "Saved Successfully";
+            return RedirectToAction("GetItem");
+           
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+
+            if (disposing) 
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
---
+++ mvcProject/Globalvariables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace mvcProject
+{
+    public static class Globalvariables
+    {
+        public static HttpClient WebApiClient = new HttpClient();
+        static Globalvariables()
+        {
+            WebApiClient.BaseAddress = new Uri("https://localhost:44301/api/");
+            WebApiClient.DefaultRequestHeaders.Clear();//First I clear the requestHeader 
+            WebApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
